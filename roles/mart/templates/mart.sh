8f65e9213c084c6a3f28c0b3209345bd62ae23ef
midmarch <midmarch@users.noreply.github.com>
2021-06-01 16:48:13 +0200

    - Initialize application.yml. - Add production profile to daemon. - Add Keycloak REALM declaration to playbook

--- roles/mart/templates/mart.sh
+++ roles/mart/templates/mart.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
 SERVICE_NAME=MART
+APP_FOLDER={{mart_application_folder}}
 PATH_TO_JAR={{mart_application_folder}}/{{mart_installer_file_name}}
 PID_PATH_NAME=/tmp/mart-pid
+JAVA_OPTIONS="-Dspring.profiles.active=prod"
+
 case $1 in
+    run)
+        echo "Running $SERVICE_NAME ..."
+        cd "$APP_FOLDER"
+        java -jar "$JAVA_OPTIONS" $PATH_TO_JAR
+    ;;
     start)
         echo "Starting $SERVICE_NAME ..."
         if [ ! -f $PID_PATH_NAME ]; then
-            nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+            cd "$APP_FOLDER"
+            nohup java -jar "$JAVA_OPTIONS" $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
             echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
@@ -33,11 +42,14 @@ case $1 in
             echo "$SERVICE_NAME stopped ...";
             rm $PID_PATH_NAME
             echo "$SERVICE_NAME starting ..."
-            nohup java -jar -Dspring.profiles.active=prod $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+            nohup java -jar "$JAVA_OPTIONS" $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
             echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
             echo "$SERVICE_NAME is not running ..."
         fi
+    ;;
+    *)
+        echo "Usage mart.sh [run|start|stop|restart]"
     ;;
 esac
